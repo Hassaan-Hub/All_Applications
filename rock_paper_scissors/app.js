cc31568039d312cac1bbcfe409efedf5cb455f21
https://github.com/Hassaan-Hub <hassaanali994@gmail.com>
2026-05-20 22:14:04 +0500

    refactor game logic for improved readability and consistency

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -9,26 +9,53 @@ const compScorePara = document.querySelector("#comp-score");
 
 
 
-const genCompChoice = (()=>{
+const genCompChoice = (() => {
     let options = ["rock", "paper", "scissors"]
     let rendInd = Math.floor(Math.random() * 3)
     return options[rendInd];
 })
 
 
-const gameDrew = (()=>{
+const gameDrew = (() => {
     msg.innerText = "Game Drew"
 })
 
 
-const showWinner = ((userWinner, userChoice, compChoice)=>{
-    if(userChoice){
+const showWinner = ((userWinner, userChoice, compChoice) => {
+    if (userWinner) {
         userScore++;
         userScorePara.innerText = userScore;
         msg.innerText = `You win! ${userChoice} beats ${compChoice}`
-    }else{
+    } else {
         compScore++;
         compScorePara.innerText = compScore;
         msg.innerText = `You win! ${compChoice} beats ${userChoice}`
     }
+})
+
+
+const playGame = ((userChoice) => {
+    let compChoice = genCompChoice()
+
+    if (userChoice === compChoice) {
+        gameDrew()
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice)
+    }
+})
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        let userChoice = choice.getAttribute("id")
+        playGame(userChoice)
+    })
 })
